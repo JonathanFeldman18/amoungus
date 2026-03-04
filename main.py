dba6d33c4d930a955e1d568d3053ebbb25d8c359
Jonathan <jonathanfeldman18@gmail.com>
2026-03-04 17:09:42 +0200

    update main for checking mongo

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from src.CRUD.mongo_crud import MongoCrud
 from src.postgress_connection import PostgresConnection
 
 if __name__ == '__main__':
-    postgres_connection = PostgresConnection()
+    #postgres_connection = PostgresConnection()
     #postgres_connection.create_new_deployment()
+
+    mongo_connection = MongoCrud()
+    #mongo_connection.create_db("temp", "temp4")
+    #mongo_connection.update_db("deqwd", "temp1")
