9dce0b5b4e3abb5b3cb8526eed50a49611efcf2a
Jonathan <jonathanfeldman18@gmail.com>
2026-03-04 16:19:51 +0200

    change id type to str:

--- src/CRUD/crud_abc.py
+++ src/CRUD/crud_abc.py
@@ -3,17 +3,17 @@ from abc import ABC, abstractmethod
 
 class CrudOnStorageTechnology(ABC):
     @abstractmethod
-    def create_db(self):
+    def create_db(self, db_name: str, username: str) -> str:
         raise NotImplementedError
 
     @abstractmethod
-    def read_db(self):
+    def read_db(self, deployment_id: str) -> dict:
         raise NotImplementedError
 
     @abstractmethod
-    def update_db(self):
+    def update_db(self, deployment_id: str, db_name: str):
         raise NotImplementedError
 
     @abstractmethod
-    def delete_db(self):
+    def delete_db(self, deployment_id: str, username: str):
         raise NotImplementedError
