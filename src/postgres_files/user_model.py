80db4d837b2e7094bb013c416d26fb58670da76b
Jonathan <jonathanfeldman18@gmail.com>
2026-03-05 21:09:38 +0200

    added user basemodel class

---
+++ src/postgres_files/user_model.py
@@ -0,0 +1,12 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+
+from src.postgres_files.permission_level import PermissionLevel
+
+
+class User(BaseModel):
+    username: str
+    password: str
+    permission_level: PermissionLevel = "READ"
+    deployment_id: UUID
