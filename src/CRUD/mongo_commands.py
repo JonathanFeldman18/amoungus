445a19f69df13f065029040b7a275818d45ec89c
Jonathan <jonathanfeldman18@gmail.com>
2026-03-05 13:45:02 +0200

    update crud functions to match mongo

--- src/CRUD/mongo_commands.py
+++ src/CRUD/mongo_commands.py
@@ -1,31 +1,34 @@
+from uuid import UUID
+
 from src.CRUD.crud_abc import CrudOnStorageTechnology
 import pymongo
 
 from src.postgress_connection import PostgresConnection
 
 
-class MongoCrud(CrudOnStorageTechnology):
+class MongocCommends(CrudOnStorageTechnology):
     def __init__(self):
         self.client = pymongo.MongoClient("mongodb://nraboy:password1234@localhost:27017/")
         super().__init__()
         self.postgres_connection = PostgresConnection()
 
-    def create_db(self, db_name: str, username: str) -> str:
+    def get_url(self) -> str:
+        return "mongodb://nraboy:password1234@localhost:27017/"
+
+    def create_db(self, db_name: str, username: str):
         new_db = self.client[db_name]
         new_db[username].insert_one({"created": db_name})
-        print(self.client.list_database_names())
 
-    def read_db(self, deployment_id: str) -> dict:
+    def read_db(self, deployment_id: UUID) -> dict:
         pass
 
-    def update_db(self, deployment_id: str, db_new_name: str):
-        for coll in self.client["temp"].list_collection_names():
-            self.client[db_new_name][coll]
-            for record in self.client["temp"][coll].find():
-                a = self.client[db_new_name][coll].insert_one(record)
-        self.client.drop_database("temp")
+    def update_db(self, old_db_name, new_db_name: str):
+        for coll in self.client[old_db_name].list_collection_names():
+            #self.client[new_db_name][coll]
+            for record in self.client[old_db_name][coll].find():
+                a = self.client[new_db_name][coll].insert_one(record)
+        self.client.drop_database(old_db_name)
 
-    def delete_db(self, deployment_id: str, username: str):
-        self.postgres_connection.get_deployment_by_id(deployment_id)
-        if
-        self.client.drop_database("temp")
+    def delete_db(self, deployment_id: UUID, username: str):
+        db_name = self.postgres_connection.get_db_name_by_id(deployment_id)
+        self.client[db_name][username].drop()
