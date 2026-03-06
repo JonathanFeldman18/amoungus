3ead2556e5cf226ad7e4005f481f3997fb7c5bc9
Jonathan <jonathanfeldman18@gmail.com>
2026-03-06 13:00:28 +0200

    updarer router to added the user role to mongo

--- src/routers/users_router.py
+++ src/routers/users_router.py
@@ -13,7 +13,7 @@ from src.my_exceptions.validate_password_exception import ValidatePasswordExcept
 from src.my_exceptions.validate_with_auth_exception import ValidateWithAuthException
 from src.postgres_files.postgress_connection import PostgresConnection
 from src.requests.user_request import User
-from src.routers.validations import validate_password, validate_username, validate_permission_level, check_full_auth
+from src.routers.validations import validate_password, validate_username, check_full_auth
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -31,6 +31,8 @@ async def create_user(user: User, credentials: Annotated[HTTPBasicCredentials, D
         check_full_auth(credentials.username, user.username, credentials.password, user.password)
         postgres_connection.check_if_user_exist(user.username)
         postgres_connection.create_new_user(user)
+        mongo_commands.add_user_to_db(user.username, user.password, user.permission_level,
+                                      postgres_connection.get_db_name_by_id(user.deployment_id))
         return JSONResponse(status_code=201, content={"message": "The user has been successfully saved."})
     except (CheckingDeploymentException, ValidatePasswordException,
             DeploymentDoesntExistException, UserExistException,
