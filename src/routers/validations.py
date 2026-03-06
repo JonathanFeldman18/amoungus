41e7acaa9ae17b580d39da5db17d9acd0f1098ff
Jonathan <jonathanfeldman18@gmail.com>
2026-03-06 10:44:21 +0200

    added validate_password, validate_permission_level, check_full_auth

--- src/routers/validations.py
+++ src/routers/validations.py
@@ -1,11 +1,23 @@
+import re
+
 from src.my_exceptions.checking_deployment_exception import CheckingDeploymentException
+from src.my_exceptions.validate_password_exception import ValidatePasswordException
 from src.my_exceptions.validate_with_auth_exception import ValidateWithAuthException
 
 
 def check_auth(login_username: str, input_username: str):
     if input_username != login_username:
         raise ValidateWithAuthException(f"The logged in username '{login_username}'"
-                                              f" does not match the one in storage - '{input_username}'.")
+                                        f" does not match the one in storage - '{input_username}'.")
+
+
+def check_full_auth(login_username: str, input_username: str, login_password: str, input_password: str):
+    if input_username != login_username:
+        raise ValidateWithAuthException(f"The logged in username '{login_username}'"
+                                        f" does not match the one in storage - '{input_username}'.")
+    if login_password != input_password:
+        raise ValidateWithAuthException(f"The logged in password does not match the one in storage")
+
 
 def validate_db_name(db_name: str):
     if ' ' in db_name:
@@ -24,3 +36,19 @@ def validate_username(username: str):
 def validate_deployment(db_name: str, username: str):
     validate_db_name(db_name)
     validate_username(username)
+
+
+def validate_permission_level(permission_level: str):
+    if permission_level.upper() not in ["READ", "READWRITE"]:
+        raise ValidateWithAuthException(f"The permission level should fit options {["READ", "READWRITE"]}.")
+
+
+def validate_password(password: str):
+    if len(password) < 8:
+        raise ValidatePasswordException("The password must contain at least 8 characters.")
+    if not re.search('[0-9]', password):
+        raise ValidatePasswordException("The password must contain at least one digit.")
+    if not re.search('[A-Z]', password):
+        raise ValidatePasswordException("The password must contain at least one uppercase letter.")
+    if not re.search('[a-z]', password):
+        raise ValidatePasswordException("The password must contain at least one lowercase letter.")
