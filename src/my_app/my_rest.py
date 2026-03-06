dc5c82c2adffc6b80bd7214eed22998d7cde9552
Jonathan <jonathanfeldman18@gmail.com>
2026-03-06 10:37:13 +0200

    added users_router to app

--- src/my_app/my_rest.py
+++ src/my_app/my_rest.py
@@ -1,13 +1,14 @@
-import uvicorn
 from fastapi import FastAPI
 import uvicorn
 
-from src.my_app import deployments_router
+from src.routers import deployments_router, users_router
+
 
 class MyApp:
     def __init__(self):
         self.app = FastAPI()
         self.app.include_router(deployments_router.router)
+        self.app.include_router(users_router.router)
 
     def run_my_app(self):
         uvicorn.run(self.app, host="0.0.0.0", port=8000)
